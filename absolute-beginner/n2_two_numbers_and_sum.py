b716f1b7c7f40edd57c7b7c802df85655ae30d6f
Vinci Sharma <vinci.sharma@gmail.com>
2021-04-24 15:09:07 +1200

    add two numbers

---
+++ absolute-beginner/n2_two_numbers_and_sum.py
@@ -0,0 +1,6 @@
+first_number = input("Please enter your first number: ")
+second_number = input("Please enter your second number: ")
+first_number = int(first_number)
+second_number = int(second_number)
+sum_of_nums = first_number + second_number
+print("The sum of the numbers is {0}".format(sum_of_nums))
